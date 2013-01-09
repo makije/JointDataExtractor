86acdb534a1fded080d39de05bcf91da9fdd128b
Martin Kiforenko Jensby <martin@rootuser.dk>
2013-01-09 21:06:38 +0100

    Move the funtionality into sub methods, write the joints names as first line in vector

--- src/JointDataExtractor.java
+++ src/JointDataExtractor.java
@@ -6,13 +6,91 @@ import java.util.ArrayList;
 
 public class JointDataExtractor {
 
+	public static void extraction(FileWriter fw, ArrayList<Frame> frames) {
+		ArrayList<String> jointTypes = new ArrayList<String>();
+		
+		for (Joint j : frames.get(0).getJoints()) {
+			if(!jointTypes.contains(j.getType()))
+				jointTypes.add(j.getType());
+		}
+		
+		for (String s : jointTypes) {
+			try {
+				fw.append(s + "\n");
+				
+				for (Frame f : frames) {
+					fw.append(f.getNumber() + "\t");
+					for (Joint j : f.getJoints()) {
+						if(j.getType().equals(s))
+							fw.append(j.getX() + "\t" + j.getY() + "\t" + j.getZ() + "\n");
+					}
+					fw.flush();
+				}
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void vector(FileWriter fw, ArrayList<Frame> frames) {
+		Frame previous = null;
+		
+		for(Joint j : frames.get(0).getJoints())
+		{
+			try {
+				fw.append(j.getType() + "\t");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		try {
+			fw.append("\n");
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		for (Frame f : frames) {
+			
+			if(previous == null)
+			{
+				for(int i = 0; i < f.getJoints().size(); i++)
+					try {
+						fw.append("0.0\t");
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+			} else {
+				for(int i = 0; i < f.getJoints().size(); i++)
+				{
+					try {
+						fw.append("" + Math.sqrt(Math.pow(f.getJoints().get(i).getX() - previous.getJoints().get(i).getX(), 2)
+								+ Math.pow(f.getJoints().get(i).getY() - previous.getJoints().get(i).getY(), 2)
+								+ Math.pow(f.getJoints().get(i).getZ() - previous.getJoints().get(i).getZ(), 2)) + "\t");
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+			
+			try {
+				fw.append("\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			previous = f;
+		}
+	}
+	
 	public static void main(String[] args) {
 		
 		String function = args[0];
 		String outputFile = args[1];
 		String dataFile = args[2];
 		
-		ArrayList<String> jointTypes = new ArrayList<String>();
+		
 		
 		File output = new File(outputFile);
 		FileWriter fw = null;
@@ -24,67 +102,13 @@ public class JointDataExtractor {
 		
 		JointParser jp = new JointParser(dataFile);
 		
-		for (Joint j : jp.getHandler().getFrames().get(0).getJoints()) {
-			if(!jointTypes.contains(j.getType()))
-				jointTypes.add(j.getType());
-		}
-		
 		if(function.equalsIgnoreCase("extraction"))
 		{
-		
-			for (String s : jointTypes) {
-				try {
-					fw.append(s + "\n");
-					
-					for (Frame f : jp.getHandler().getFrames()) {
-						fw.append(f.getNumber() + "\t");
-						for (Joint j : f.getJoints()) {
-							if(j.getType().equals(s))
-								fw.append(j.getX() + "\t" + j.getY() + "\t" + j.getZ() + "\n");
-						}
-						fw.flush();
-					}
-					
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		
-		} else if(function.equalsIgnoreCase("vector"))
+			extraction(fw, jp.getHandler().getFrames());
+		} 
+		else if(function.equalsIgnoreCase("vector"))
 		{
-			Frame previous = null;
-			for (Frame f : jp.getHandler().getFrames()) {
-				
-				if(previous == null)
-				{
-					for(int i = 0; i < f.getJoints().size(); i++)
-						try {
-							fw.append("0.0\t");
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-				} else {
-					for(int i = 0; i < f.getJoints().size(); i++)
-					{
-						try {
-							fw.append("" + Math.sqrt(Math.pow(f.getJoints().get(i).getX() - previous.getJoints().get(i).getX(), 2)
-									+ Math.pow(f.getJoints().get(i).getY() - previous.getJoints().get(i).getY(), 2)
-									+ Math.pow(f.getJoints().get(i).getZ() - previous.getJoints().get(i).getZ(), 2)) + "\t");
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-				
-				try {
-					fw.append("\n");
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				
-				previous = f;
-			}
-			
+			vector(fw, jp.getHandler().getFrames());
 		}
 	}
 	
